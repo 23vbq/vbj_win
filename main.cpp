d34463acce6fbf6e5e5af08b3c2deaebb09b74fa
23vbq <vblackgp123@gmail.com>
2023-04-12 19:03:22 +0200

    prints player hand, dealer hand and decisions

--- main.cpp
+++ main.cpp
@@ -12,36 +12,56 @@ using namespace std;
 
 HANDLE hConsole;
 bool game_loop = true;
-char input;
+int input;
 
 Card card_deck[52];
 const char* type_icon[] = {"♣", "♦", "♠", "♥"};
+const char* special_card_names[] = {"J", "Q", "K", "A"};
 vector<int> shuffle;
 
 vector<Card*> player_hand;
 vector<Card*> dealer_hand;
 
+void getinput();
 void SetupDeck();
 void ShuffleDeck();
 template<typename T>
 bool VectorContains(vector<T> v, T value);
 void GiveCard(vector<Card*> &hand);
+void PrintCards(vector<Card*> hand, const char* title, bool printAll);
+void PrintDecisions();
 void Test();
+//void signalHandler(int signum);
 //void FreeMem();
 
 int main(){
+    //signal(SIGINT, signalHandler);
+    // Setup game
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleOutputCP(CP_UTF8);
     srand(time(NULL));
     SetupDeck();
     
+    // Game start
     while(game_loop){
+        system("cls");
         if(shuffle.size() == 0) ShuffleDeck();
+        player_hand.clear();
+        dealer_hand.clear();
         for(int i = 0; i < 2; i++)
             GiveCard(player_hand);
         for(int i = 0; i < 2; i++)
             GiveCard(dealer_hand);
-        printf("Debug: %d\n", player_hand.size());
+
+        bool game_ends = false;
+        while(!game_ends){
+            //system("cls");
+            printf("Input was: %d\n\n", input);
+            PrintCards(dealer_hand, "Dealer", false);
+            PrintCards(player_hand, "Player", true);
+            PrintDecisions();
+            getinput();
+        }
         for(auto p : player_hand){
             printf("%d - %d\n", p->GetValue(), p->Type);
         }
@@ -54,6 +74,10 @@ int main(){
     return 0;
 }
 
+void getinput(){
+    input = _getch();
+    if(input == 3) exit(1);
+}
 void SetupDeck(){
     Card * p;
     for(int i = 0; i < 4; i++){
@@ -89,6 +113,29 @@ void GiveCard(vector<Card*> &hand){
     hand.push_back(p);
     shuffle.pop_back();
 }
+void PrintCards(vector<Card*> hand, const char* title, bool printAll){
+    printf("%*s%*s\n", 10 + strlen(title)/2, title, 10 - strlen(title)/2, "");
+    for(int i = 0; i < hand.size(); i++){
+        if(i != 0 && !printAll){
+            printf("%3s###", "");
+            continue;
+        }
+        const char* cardName;
+        if(hand[i]->Value < 11) cardName = to_string(hand[i]->Value).c_str();
+        else cardName = special_card_names[hand[i]->Value - 11];
+        printf("%3s%s %s", "", cardName, type_icon[hand[i]->Type]);
+    }
+    printf("\n");
+}
+void PrintDecisions(){
+    int k = 1;
+    printf("\n%d. Hit\n", k++);
+    printf("%d. Stand\n", k++);
+    if(player_hand.size() == 2){
+        if(player_hand[0]->Value == player_hand[1]->Value) printf("%d. Split\n", k++);
+        if(dealer_hand[0]->Value == 14) printf("%d. Insurance\n", k++);
+    } 
+}
 void Test(){
     int index;
         while(true){
@@ -99,6 +146,10 @@ void Test(){
         }
     cout<<"Closing...";
 }
+/*void signalHandler(int signum){
+    printf("Ch");
+    exit(signum);
+}*/
 /*void FreeMem(){
     for(int i = 0; i < 52; i++){
         delete card_deck[i];
